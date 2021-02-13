72f87275980c9b3be244fad0c1fce8ea78e4a658
Justin Chellah <jaychellah@ugaming.org>
2021-02-13 10:02:07 +0100

    Update smsdk_config.h

--- smsdk_config.h
+++ smsdk_config.h
@@ -5,7 +5,7 @@
 #define SMEXT_CONF_DESCRIPTION	"Improves behavior of survivor bots when collecting objects in environment"
 #define SMEXT_CONF_VERSION		"1.0.0"
 #define SMEXT_CONF_AUTHOR		"Jay"
-#define SMEXT_CONF_URL			"http://ugaming.org"
+#define SMEXT_CONF_URL			"https://github.com/jaychellah/survivor_object_collection"
 #define SMEXT_CONF_LOGTAG		"L4D2-SOC"
 #define SMEXT_CONF_LICENSE		"MIT"
 #define SMEXT_CONF_DATESTRING	__DATE__

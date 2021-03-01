6ec44866c48e162dd971a18823dd16efbfd23368
Justin Chellah <jaychellah@ugaming.org>
2021-03-01 15:07:05 +0100

    changed naming conventions

--- extension.h
+++ extension.h
@@ -12,23 +12,23 @@ class SurvivorTeamSituation;
 // forward declaration
 template < typename Actor > class Action;
 
-inline CBaseEntity *EntityFromBaseHandle(void *addr, int offset)
+inline CBaseEntity *EntityFromBaseHandle(void *pAddress, int offset)
 {
-    CBaseHandle &hndl = *reinterpret_cast<CBaseHandle *>(reinterpret_cast<char *>(addr) + offset);
-    CBaseEntity *handleEntity = gamehelpers->ReferenceToEntity(hndl.GetEntryIndex());
+	CBaseHandle &hndl = *reinterpret_cast<CBaseHandle *>(reinterpret_cast<byte *>(pAddress) + offset);
+	CBaseEntity *pHandleEntity = gamehelpers->ReferenceToEntity(hndl.GetEntryIndex());
 
-    if (!handleEntity || hndl != reinterpret_cast<IHandleEntity *>(handleEntity)->GetRefEHandle())
-    {
-        return NULL;
-    }
+	if (!pHandleEntity || hndl != reinterpret_cast<IHandleEntity *>(pHandleEntity)->GetRefEHandle())
+	{
+		return NULL;
+	}
 	
-    return handleEntity;
+	return pHandleEntity;
 }
 
 class CSurvivorObjectCollectionExt : public SDKExtension
 {
 public:
-    /**
+	/**
 	 * @brief This is called after the initial loading sequence has been processed.
 	 *
 	 * @param error		Error message buffer.
@@ -36,20 +36,20 @@ public:
 	 * @param late		Whether or not the module was loaded after map load.
 	 * @return			True to succeed loading, false to fail.
 	 */
-    virtual bool SDK_OnLoad(char *error, size_t maxlen, bool late);
+	virtual bool SDK_OnLoad(char *error, size_t maxlen, bool late);
 
-    /**
+	/**
 	 * @brief This is called right before the extension is unloaded.
 	 */
-    virtual void SDK_OnUnload();
+	virtual void SDK_OnUnload();
 
-    /**
+	/**
 	 * @brief This is called once all known extensions have been loaded.
 	 * Note: It is is a good idea to add natives here, if any are provided.
 	 */
-    virtual void SDK_OnAllLoaded();
+	virtual void SDK_OnAllLoaded();
 #ifdef SMEXT_CONF_METAMOD
-    /**
+	/**
 	 * @brief Called when Metamod is attached, before the extension version is called.
 	 *
 	 * @param error			Error buffer.
@@ -57,18 +57,18 @@ public:
 	 * @param late			Whether or not Metamod considers this a late load.
 	 * @return				True to succeed, false to fail.
 	 */
-    virtual bool SDK_OnMetamodLoad(ISmmAPI *ismm, char *error, size_t maxlen, bool late);
+	virtual bool SDK_OnMetamodLoad(ISmmAPI *ismm, char *error, size_t maxlen, bool late);
 #endif
-    /**
+	/**
 	 * @brief Return false to tell Core that your extension should be considered unusable.
 	 *
 	 * @param error				Error buffer.
 	 * @param maxlength			Size of error buffer.
 	 * @return					True on success, false otherwise.
 	 */
-    virtual bool QueryRunning(char *error, size_t maxlen);
+	virtual bool QueryRunning(char *error, size_t maxlen);
 
-    /**
+	/**
 	 * @brief Asks the extension whether it's safe to remove an external 
 	 * interface it's using.  If it's not safe, return false, and the 
 	 * extension will be unloaded afterwards.
@@ -86,7 +86,7 @@ public:
 	 */
 	virtual bool QueryInterfaceDrop(SMInterface *pInterface);
 
-    /**
+	/**
 	 * @brief Notifies the extension that an external interface it uses is being removed.
 	 *
 	 * @param pInterface		Pointer to interface being dropped.  This
@@ -96,52 +96,52 @@ public:
 	 */
 	virtual void NotifyInterfaceDrop(SMInterface *pInterface);
 
-    int GetAmmoCount(CBaseEntity *player, int ammoIndex) { return *reinterpret_cast<int *>(reinterpret_cast<byte *>(player) + sendprop_m_iAmmo + (ammoIndex * 4)); }
-    int GetPrimaryAmmoType(CBaseEntity *weapon);
-    int GetMaxCarry(const char *wpnName);
+	int GetAmmoCount(CBaseEntity *pPlayer, int iAmmo) { return *reinterpret_cast<int *>(reinterpret_cast<byte *>(pPlayer) + m_iSendProp_CTerrorPlayer_m_iAmmo + (iAmmo * 4)); }
+	int GetPrimaryAmmoType(CBaseEntity *pWeapon);
+	int GetMaxCarry(const char *pWeaponName);
 
-    int GetTankCount() { return *reinterpret_cast<int *>(reinterpret_cast<byte *>(addr_TheDirector) + offset_CDirector_m_iTankCount); }
+	int GetTankCount() { return *reinterpret_cast<int *>(reinterpret_cast<byte *>(m_pObj_CDirector_TheDirector) + m_iCDirector_m_iTankCount); }
 
-    float GetTimeSinceAttackedByEnemy(CBaseEntity *player);
+	float GetTimeSinceAttackedByEnemy(CBaseEntity *pPlayer);
 
-    CBaseEntity *GetFriendIntrouble(SurvivorTeamSituation *situation) { return EntityFromBaseHandle(situation, offset_SurvivorTeamSituation_m_friendInTrouble); }
-    CBaseEntity *GetTonguedFriend(SurvivorTeamSituation *situation) { return EntityFromBaseHandle(situation, offset_SurvivorTeamSituation_m_tonguedFriend); }
-    CBaseEntity *GetPouncedFriend(SurvivorTeamSituation *situation) { return EntityFromBaseHandle(situation, offset_SurvivorTeamSituation_m_pouncedFriend); }
-    CBaseEntity *GetPummeledFriend(SurvivorTeamSituation *situation) { return EntityFromBaseHandle(situation, offset_SurvivorTeamSituation_m_pummeledFriend); }
+	CBaseEntity *GetFriendIntrouble(SurvivorTeamSituation *pSituation) { return EntityFromBaseHandle(pSituation, m_iSurvivorTeamSituation_m_friendInTrouble); }
+	CBaseEntity *GetTonguedFriend(SurvivorTeamSituation *pSituation) { return EntityFromBaseHandle(pSituation, m_iSurvivorTeamSituation_m_tonguedFriend); }
+	CBaseEntity *GetPouncedFriend(SurvivorTeamSituation *pSituation) { return EntityFromBaseHandle(pSituation, m_iSurvivorTeamSituation_m_pouncedFriend); }
+	CBaseEntity *GetPummeledFriend(SurvivorTeamSituation *pSituation) { return EntityFromBaseHandle(pSituation, m_iSurvivorTeamSituation_m_pummeledFriend); }
 
-    CBaseEntity *GetUseObject(Action<CBaseEntity> *action) { return EntityFromBaseHandle(action, offset_SurvivorCollectObject_m_useObject); }
+	CBaseEntity *GetUseObject(Action<CBaseEntity> *pAction) { return EntityFromBaseHandle(pAction, m_iSurvivorCollectObject_m_useObject); }
 
-    CBaseEntity *Weapon_GetSlot(CBaseEntity *player, int slot);
+	CBaseEntity *Weapon_GetSlot(CBaseEntity *pPlayer, int slot);
 
-    SurvivorTeamSituation *GetTeamSituation(CBaseEntity *bot);
+	SurvivorTeamSituation *GetTeamSituation(CBaseEntity *pBot);
 
-    bool ShouldGiveUp(Action<CBaseEntity> *action, CBaseEntity *bot);
+ 	bool ShouldGiveUp(Action<CBaseEntity> *pAction, CBaseEntity *pBot);
 
 private:
-    IBinTools *bintools = NULL;
+ 	IBinTools *m_pBinTools = NULL;
 
-    int sendprop_m_iAmmo = -1;
+	int m_iSendProp_CTerrorPlayer_m_iAmmo = -1;
 
-    int offset_SurvivorTeamSituation_m_friendInTrouble = -1;
-    int offset_SurvivorTeamSituation_m_tonguedFriend = -1;
-    int offset_SurvivorTeamSituation_m_pouncedFriend = -1;
-    int offset_SurvivorTeamSituation_m_pummeledFriend = -1;
-    int offset_SurvivorCollectObject_m_useObject = -1;
-    int offset_CTerrorPlayer_m_timeSinceAttackedByEnemyTimer = -1;
-    int offset_CDirector_m_iTankCount = -1;
+	int m_iSurvivorTeamSituation_m_friendInTrouble = -1;
+	int m_iSurvivorTeamSituation_m_tonguedFriend = -1;
+	int m_iSurvivorTeamSituation_m_pouncedFriend = -1;
+	int m_iSurvivorTeamSituation_m_pummeledFriend = -1;
+	int m_iSurvivorCollectObject_m_useObject = -1;
+	int m_iCTerrorPlayer_m_timeSinceAttackedByEnemyTimer = -1;
+	int m_iCDirector_m_iTankCount = -1;
 
-    int vtblindex_CBaseCombatCharacter_Weapon_GetSlot = -1;
+	int m_iVtblIndex_CBaseCombatCharacter_Weapon_GetSlot = -1;
 
-    CDetour *detour_SurvivorCollectObject_ShouldGiveUp = NULL;
+	CDetour *m_pDetour_SurvivorCollectObject_ShouldGiveUp = NULL;
 
-    void *pfn_SurvivorBot_GetTeamSituation = NULL;
-    void *pfn_SurvivorUseObject_ShouldGiveUp = NULL;
+	void *m_pfn_SurvivorBot_GetTeamSituation = NULL;
+	void *m_pfn_SurvivorUseObject_ShouldGiveUp = NULL;
 
-    void *addr_TheDirector = NULL;
+	void *m_pObj_CDirector_TheDirector = NULL;
 
-    ICallWrapper *vcall_CBaseCombatCharacter_Weapon_GetSlot = NULL;
-    ICallWrapper *call_SurvivorBot_GetTeamSituation = NULL;
-    ICallWrapper *call_SurvivorUseObject_ShouldGiveUp = NULL;
+	ICallWrapper *m_pCallWrap_CBaseCombatCharacter_Weapon_GetSlot = NULL;
+	ICallWrapper *m_pCallWrap_SurvivorBot_GetTeamSituation = NULL;
+	ICallWrapper *m_pCallWrap_SurvivorUseObject_ShouldGiveUp = NULL;
 };
 
 #endif // _INCLUDE_SURVIVOR_OBJECT_COLLECTION_EXTENSION_PROPER_H_
